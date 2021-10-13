2983a78e00abdad34baf5f004a9ba1e4c051f6f9
tim <taemon1337@gmail.com>
2021-10-13 09:51:53 -0400

    rename script compiles

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 )
 
 replace (
+	k8s.io/platform-controller => ../platform-controller
 	k8s.io/api => k8s.io/api v0.0.0-20211001003357-dd4141958dfc
 	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20211001003147-df63df3af3fc
 	k8s.io/client-go => k8s.io/client-go v0.0.0-20211001003700-dbfa30b9d908
--- hack/rename-sample.sh
+++ hack/rename-sample.sh
@@ -8,4 +8,14 @@ find ./pkg -type f -exec sed -i -e "s/Foo/${Foo}/g" {} \;
 find ./pkg -type f -exec sed -i -e "s/foo/${foo}/g" {} \;
 #find ./pkg -type f -exec sed -i -e "s/example/${example}/g" {} \;
 
+# rename files
+mv ./pkg/generated/clientset/versioned/typed/samplecontroller/v1alpha1/foo.go ./pkg/generated/clientset/versioned/typed/${sample}controller/v1alpha1/${foo}.go
+mv ./pkg/generated/clientset/versioned/typed/samplecontroller/v1alpha1/samplecontroller_client.go ./pkg/generated/clientset/versioned/typed/${sample}controller/v1alpha1/${sample}controller_client.go
+mv ./pkg/generated/informers/externalversions/samplecontroller/v1alpha1/foo.go ./pkg/generated/informers/externalversions/samplecontroller/v1alpha1/${foo}.go
+mv ./pkg/generated/listers/samplecontroller/v1alpha1/foo.go ./pkg/generated/listers/${sample}controller/v1alpha1/${foo}.go
 
+# rename dirs
+mv ./pkg/generated/informers/externalversions/samplecontroller ./pkg/generated/informers/externalversions/${sample}controller
+mv ./pkg/generated/listers/samplecontroller ./pkg/generated/listers/${sample}controller
+mv ./pkg/generated/clientset/versioned/typed/samplecontroller ./pkg/generated/clientset/versioned/typed/${sample}controller
+mv ./pkg/apis/samplecontroller ./pkg/apis/${sample}controller
